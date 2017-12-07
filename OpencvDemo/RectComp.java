2c1edc7f1fd6bf363b89512493c6f595a1dd61dd
edisonlao <350482257@qq.com>
2017-12-07 10:36:13 +0800

    add RectComp

---
+++ OpencvDemo/RectComp.java
@@ -0,0 +1,18 @@
+package com.sendroids.opencv;
+
+import org.opencv.core.Rect;
+
+public class RectComp {
+    Rect rm;
+
+    RectComp(Rect rms) {
+        rm = rms;
+    }
+
+    final boolean operator (RectComp ti)
+    {
+        return rm.x == ti.rm.x;
+    }
+
+
+}
